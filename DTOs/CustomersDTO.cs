086a8e7aa6d298b5402695644097f33fff64e1eb
sukanta_cr7 <sukantamajhi777@gmail.com>
2025-11-14 18:22:24 +0530

    Implement data annotation

--- DTOs/CustomersDTO.cs
+++ DTOs/CustomersDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace Dot_Net_Core_Tutorial.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dot_Net_Core_Tutorial.DTOs
 {
     public class CustomersDTO
     {
+        [Required,MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        [Required,EmailAddress,MaxLength(50)]
         public string Email { get; set; } = string.Empty;
+        [Required,Phone,MaxLength(10)]
         public string Phone { get; set; } = string.Empty;
+        [MaxLength(250)]
         public string Address { get; set; } = string.Empty;
 
     }
--- DTOs/LoginDetailsDTO.cs
+++ DTOs/LoginDetailsDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace Dot_Net_Core_Tutorial.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dot_Net_Core_Tutorial.DTOs
 {
     public class LoginDetailsDTO
     {
+        [Required,MaxLength(50)]
         public string UserName { get; set; } = string.Empty;
+        [Required,MaxLength(20)]
         public string Password { get; set; } = string.Empty;
     }
 }
